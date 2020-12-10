ae33fb7180d6fc0581fd010a7036ca675c6d56ea
nabin hyamikha <hyanmikhanabin@gmail.com>
2020-12-11 00:38:51 +0545

    update the portfolio v2

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,4 @@
-/*!
-    * Start Bootstrap - Resume v6.0.2 (https://startbootstrap.com/theme/resume)
-    * Copyright 2013-2020 Start Bootstrap
-    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
-    */
+
     (function ($) {
     "use strict"; // Start of use strict
 
@@ -40,3 +36,49 @@
         target: "#sideNav",
     });
 })(jQuery); // End of use strict
+
+
+
+
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxX1lNEDuDWpj8q7oDwJiX9q0UXem1HgTMIfaixyiI8NHcj7L8/exec'
+            const form = document.forms['google-sheet']
+          
+            form.addEventListener('submit', e => {
+              e.preventDefault()
+              fetch(scriptURL, { method:'post', body: new FormData(form)})
+                // .then(response => alert("Thanks for Contacting us..! We Will Contact You Soon..."))
+                .then(response => toster())
+                .catch(error => errortoster())
+            })
+            function toster(){
+                var x = document.getElementById("snackbar");
+                document.getElementById("gform").reset();
+                x.className = "show";
+                setTimeout(function(){x.className=x.className.replace("show", "");},3000);
+
+            }
+
+            function errortoster(){
+
+                var x = document.getElementById("errorsnackbar");
+                document.getElementById("gform").reset();
+                x.className = "show";
+                setTimeout(function(){x.className=x.className.replace("show", "");},3000);
+            }
+            
+
+
+$(document).ready(function(){
+        $(".btn-light").click(function(){
+            $(this).addClass("active1");
+            setTimeout(function(){
+                $(".btn-light").addClass("success1");
+            }, 3700);
+
+            setTimeout(function(){
+                $(".btn-light").removeClass("active1");
+                $(".btn-light").removeClass("success1");
+            }, 5000);
+        });
+    });
